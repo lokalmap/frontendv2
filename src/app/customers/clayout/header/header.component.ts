b9b9146f6adb1cb98f09cb58eba6c5ef72a96970
LFLaquio <louie.laquio@gmail.com>
2018-03-04 17:46:11 +0800

    added JSON transform Function for parsing under map.service.ts; added test variable to test the parsing at clayout/header/header.component.ts

--- src/app/customers/clayout/header/header.component.ts
+++ src/app/customers/clayout/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Gvar } from '../../../var';
+import { MapFastDB } from '../../../_models';
+import { MapService } from '../../../services/map.service';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
@@ -11,7 +13,7 @@ export class HeaderComponent implements OnInit {
 
     pushRightClass: string = 'push-right';
 
-    constructor(private translate: TranslateService, public router: Router) {
+    constructor(private translate: TranslateService, public router: Router,private mapsrv: MapService) {
         this.router.events.subscribe((val) => {
             if (val instanceof NavigationEnd && window.innerWidth <= 992 && this.isToggled()) {
                 this.toggleSidebar();
@@ -45,7 +47,77 @@ export class HeaderComponent implements OnInit {
         this.translate.use(language);
     }
     onSubmitFN(stextv?:string){
-      Gvar.v1 = stextv;
-      console.log("test value " + stextv);
+      //Gvar.v1 = stextv;
+      //console.log("test value " + stextv);
+        let testvarx = {
+        	"x": "XXXX.FFFF",
+        	"li": {
+        		"banana": [{
+        			"y": "YYYY.FFF1",
+              "x": "XXXX.FFF2",
+              "pID": "Provider ID asd",
+        			"iID": "Item ID 123",
+              "price":1,
+              "itmR":3,
+        			"promo": ["datef(MM,DD,YY,hh,mm)", "Date2(MM,DD,YY,hh,mm)", "metadataURL"]
+        		}, {
+        			"y": "YYYY.FFF23",
+              "x": "XXXX.FFF2",
+              "pID": "Provider ID asd",
+              "iID": "Item ID 123",
+              "price":1,
+              "itmR":3,
+              "promo": ["datef(MM,DD,YY,hh,mm)", "Date2(MM,DD,YY,hh,mm)", "metadataURL"]
+        		}],
+        		"apple": [{
+              "y": "YYYY.FFF13q",
+              "x": "XXXX.FFF2qq",
+              "pID": "Provider ID asdq2wd",
+        			"iID": "Item ID 613231122",
+              "price":1,
+              "itmR":3,
+        			"promo": ["datef(MM,DD,YY,hh,mm)", "Date2(MM,DD,YY,hh,mm)", "metadataURL"]
+        		}, {
+              "y": "YYYY.FFFsa1",
+              "x": "XXXX.FFFs2",
+              "pID": "Provider ID 6889asd",
+        			"iID": "Item ID 90876123",
+              "price":1,
+              "itmR":3,
+        			"promo": ["datef(MM,DD,YY,hh,mm)", "Date2(MM,DD,YY,hh,mm)", "metadataURL"]
+        		}]
+        	},
+        	"ads": [{
+        			"name": "storename1;URL1",
+        			"Desc": "Description Here",
+        			"Tags": "aaa;bbb;ccc",
+        			"MetaD": {
+        				"data1": [1, 2, 3],
+        				"data2": [4, 5, 6]
+        			}
+        		},
+        		{
+        			"name": "storename1;URL1",
+        			"Desc": "Description Here",
+        			"Tags": "aaa;bbb;ccc",
+        			"MetaD": {
+        				"data1": [1, 2, 3],
+        				"data2": [4, 5, 6]
+        			}
+        		}
+        	],
+        	"premprov": ["prov1", "prov2", "prov3"],
+        	"lstupd": "2018-02-19T15:14:29.646Z",
+        	"providerctr": 0
+        }
+      //console.log("testv",testvarx);
+      let testobj:any[] = this.mapsrv.transFN(testvarx);
+      for (let item in testobj){
+        console.log("val: " + item, testobj[item]);
+      }
+
+      console.log("final: ",testobj);
+
+//      console.log("final2: ", createCacheDB());
     }
 }
--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -3,6 +3,24 @@ import { Http, Headers, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { MapFastDB } from '../_models';
 
+interface CacheDBItm{
+  name?:string;
+  x?:string;
+  y?:string;
+  pID?:string;
+  iID?:string;
+  price?:number;
+  promo?:any;
+  itmR?:number;
+}
+interface CacheDBItmGrp{
+  itemGrpName:string;
+  cDBList:CacheDBItm[];
+}
+interface CacheDBInfo{
+  id:string;
+  Info:any[];
+}
 @Injectable()
 export class MapService {
     constructor(private http: Http) { console.log('Data service connected...')}
@@ -10,7 +28,6 @@ export class MapService {
       'Content-Type' : 'application/json',
       'Authorization': localStorage.getItem("beJWT")
     });
-
 
     getAll(): Observable<any> {
         console.log("get array");
@@ -36,4 +53,65 @@ export class MapService {
         return Observable.throw(err);
       });
     }
+    transFN(mapdb: any):any{
+      let xh:string;
+      let objtmp:any;
+      let retval:any[]=[];
+      let cdbtemp:CacheDBItm;
+      for (let itmxid in mapdb){
+//        console.log("line: ",typeof itmxid, itmxid, mapdb[itmxid]);
+//        console.log("testv : " + '{"' + itmxid + '":"' + mapdb[itmxid] + '"}');
+        switch (itmxid)
+        {
+            case "x":{
+              xh =  mapdb[itmxid];
+              break;
+            }
+            case "li":{
+              objtmp = mapdb[itmxid];
+              for (let itm in objtmp){
+                let ctmp: CacheDBItmGrp={itemGrpName:"",cDBList:[]};
+                ctmp.itemGrpName = itm;
+                let cdbtemp:CacheDBItm = {name:"",x:"",y:"",pID:"",iID:"",price:0,promo:{},itmR:0}
+                updXCacheDB(cdbtemp,JSON.parse('{"name":"' + itm + '"}'));
+                let itmobj = objtmp[itm]
+                for (let j=0,len=itmobj.length;j<len;++j){
+                  let dtls:any = itmobj[j];
+                  for (let dtl in dtls){
+                      updXCacheDB(cdbtemp,JSON.parse('{"' + dtl + '":"' + dtls[dtl] + '"}'));
+                  }
+                  ctmp.cDBList.push(cdbtemp);
+                }
+                console.log("view: ",ctmp);
+                retval.push(ctmp);
+              }
+              break;
+            }
+        }
+      }
+      return retval;
+    }
+}
+/*
+function createCacheDB(cdbconfig: CachedbItm):{name?:string; x?:string; y?:string; sID?:string; iID?:string; price?:number; promo?:any; itmR?:number}{
+      let newcdb:CachedbItm;
+      if (cdbconfig.name){ newcdb.name = cdbconfig.name; }
+      if (cdbconfig.x){ newcdb.x = cdbconfig.x; }
+      if (cdbconfig.y){ newcdb.y = cdbconfig.y; }
+      if (cdbconfig.sID){ newcdb.sID = cdbconfig.sID; }
+      if (cdbconfig.iID){ newcdb.iID = cdbconfig.iID; }
+      if (cdbconfig.price){ newcdb.price = cdbconfig.price; }
+      if (cdbconfig.itmR){ newcdb.itmR = cdbconfig.itmR; }
+      return newcdb;
+}
+*/
+function updXCacheDB(varref:CacheDBItm,cdbconfig: CacheDBItm){
+  if (cdbconfig.name){ varref.name = cdbconfig.name; }
+  if (cdbconfig.x){ varref.x = cdbconfig.x; }
+  if (cdbconfig.y){ varref.y = cdbconfig.y; }
+  if (cdbconfig.pID){ varref.pID = cdbconfig.pID; }
+  if (cdbconfig.iID){ varref.iID = cdbconfig.iID; }
+  if (cdbconfig.price){ varref.price = cdbconfig.price; }
+  if (cdbconfig.promo){ varref.promo = cdbconfig.promo; }
+  if (cdbconfig.itmR){ varref.itmR = cdbconfig.itmR; }
 }
