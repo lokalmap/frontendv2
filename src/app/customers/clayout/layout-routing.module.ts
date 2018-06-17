20d1ebc8afa406d81bd0d62f27f5c69b219d4ccf
LFLaquio <louie.laquio@gmail.com>
2018-06-17 18:28:34 +0800

    Phase 2, migration from Angular 4 to Angular 6

--- src/app/customers/clayout/layout-routing.module.ts
+++ src/app/customers/clayout/layout-routing.module.ts
@@ -11,14 +11,14 @@ const routes: Routes = [
             { path: 'history', loadChildren: '../history/history.module#HistoryModule' },
             { path: 'chat', loadChildren: '../cchat/cchat.module#CchatModule' },
             { path: 'track', loadChildren: '../../shared/components/track/track.module#TrackModule' },
-            { path: 'dashboard', loadChildren: '../../layout/dashboard/dashboard.module#DashboardModule' },
-            { path: 'charts', loadChildren: '../../layout/charts/charts.module#ChartsModule' },
-            { path: 'tables', loadChildren: '../../layout/tables/tables.module#TablesModule' },
-            { path: 'bs-element', loadChildren: '../../layout/bs-element/bs-element.module#BsElementModule' },
-            { path: 'grid', loadChildren: '../../layout/grid/grid.module#GridModule' },
-            { path: 'form', loadChildren: '../../layout/form/form.module#FormModule' },
-            { path: 'components', loadChildren: '../../layout/bs-component/bs-component.module#BsComponentModule' },
-            { path: 'blank-page', loadChildren: '../../layout/blank-page/blank-page.module#BlankPageModule' },
+//            { path: 'dashboard', loadChildren: '../../layout/dashboard/dashboard.module#DashboardModule' },
+//            { path: 'charts', loadChildren: '../../layout/charts/charts.module#ChartsModule' },
+//            { path: 'tables', loadChildren: '../../layout/tables/tables.module#TablesModule' },
+//            { path: 'bs-element', loadChildren: '../../layout/bs-element/bs-element.module#BsElementModule' },
+//            { path: 'grid', loadChildren: '../../layout/grid/grid.module#GridModule' },
+//            { path: 'form', loadChildren: '../../layout/form/form.module#FormModule' },
+//            { path: 'components', loadChildren: '../../layout/bs-component/bs-component.module#BsComponentModule' },
+//            { path: 'blank-page', loadChildren: '../../layout/blank-page/blank-page.module#BlankPageModule' },
         ]
     }
 ];
